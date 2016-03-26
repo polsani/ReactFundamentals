05c8f03a55373750fead856eba7be2099cd980e6
Henrique Polsani <henrique.polsani@gmail.com>
2016-03-26 16:17:44 -0300

    introduce result layout and refactor components

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -3,16 +3,12 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-function puke(object){
-    console.log(object);
-    return <pre>{JSON.stringify(object,null, ' ')}</pre>
-}
+var MainContainer = require('../components/MainContainer');
 
 function ConfirmBattle(props){
     return props.isLoading === true ?
         <p>Loading</p> :
-        <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <MainContainer>
             <h1>Confirm Players</h1>
             <div className="col-sm-8 col-sm-offset-2">
                 <UserDetailsWrapper header="Player 1">
@@ -32,7 +28,7 @@ function ConfirmBattle(props){
                     </Link>
                 </div>
             </div>
-        </div>
+        </MainContainer>
 };
 
 ConfirmBattle.propTypes = {
